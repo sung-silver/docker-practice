cc77f7df36fe881350125a6e2f5fabb2935b6c38
sung-silver <jinna0319@gmail.com>
2024-11-08 16:14:56 +0900

    [WEEK2] 다중 컨테이너를 식별하기 위해 UUID 변수 추가

--- src/main/java/com/docker/practice/HelloController.java
+++ src/main/java/com/docker/practice/HelloController.java
@@ -1,11 +1,14 @@
 package com.docker.practice;
 
+import java.util.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+    private final String uuid = UUID.randomUUID().toString();
+
     @GetMapping("/hello")
     public String getHello() {
-        return "Hello";
+        return "Hello, My UUID is "+uuid;
     }
 }
